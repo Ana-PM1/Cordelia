684179489e2d346d2ba0517c146e990f388477f8
Ana-PM1 <akarenpm1@gmail.com>
2025-07-16 14:08:31 -0600

    Animaciones, Colliders

--- Assets/Scripts/CameraFollows.cs
+++ Assets/Scripts/CameraFollows.cs
@@ -6,10 +6,18 @@ public class CameraFollows : MonoBehaviour
 {
     public Transform target; // El jugador
     public Vector3 offset; // Desplazamiento de la cámara
+
+    public float minY = -5f; // Límite inferior
+    public float maxY = 10f; // Límite superior
+
     void LateUpdate()
     {
-        // Calcula la nueva posición de la cámara
+        // Calcula la nueva posición deseada de la cámara
         Vector3 desiredPosition = target.position + offset;
+
+        // Limita la posición en el eje Y
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, minY, maxY);
+
         // Suaviza el movimiento de la cámara
         transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5);
     }
